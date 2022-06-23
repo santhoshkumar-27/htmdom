d2a783c4ade2762c574b3836787c3b1788bd2d3b
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-06-23 19:41:46 +0530

    feat: Study the innerhtml vs createElement

--- assets/learning.js
+++ assets/learning.js
@@ -22,3 +22,12 @@ const message = document.querySelector('.alert-success');
 message.innerText = '<div>Succesfully updated</div>';
 message.innerHTML = '<div>Succesfully updated</div>';
 message.textContent = '<div>Succesfully updated</div>';
+const content = document.getElementById('content');
+const innerhtml = `
+    <section>
+        dummy using innerHtml
+    </section>
+`;
+// inner html is string type 
+// but using the createElement is object type so we have lot of freedom to do rather than in innerHTML
+content.innerHTML += innerhtml;
