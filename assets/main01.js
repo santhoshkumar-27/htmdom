dcb231fcd063465c451634b72b62ddce3b4e5bb9
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-07-12 09:38:12 +0530

    feat: learned javascript events

--- assets/main01.js
+++ assets/main01.js
@@ -1,54 +1,73 @@
-// let values = [];
-// let textFieldValue = document.getElementById('text-value');
-// let ul = document.getElementById('answershow')
-// let foodItems = ul.getElementsByClassName('food-item')
-// // console.log('ul', ul.parentNode.insertBefore(li, ul))
-// function printValue() {
-//     if (textFieldValue.value) {
-//         let li = document.createElement('li');
-//         let span = document.createElement('span');
-//         let div = document.createElement('div');
-//         div.innerText = 'X';
-//         div.className = 'click';
-//         // div.setAttribute('onclick', 'removeItem(event)'); //for clicking attribure
-//         span.innerText = textFieldValue.value;
-//         li.classList = 'food-item';
-//         li.append(span, div);
-//         // li.appendChild(document.createTextNode(textFieldValue.value))
-//         // ul.appendChild(li);
-//         ul.append(li);// append means adding element to lastchild
-//         // ul.prepend(li) //prepend means adding element to firstchild
-//         // ul.before(li) //adding element before the any element
-//         // ul.after(li) // adding element after the any element
-//         // ul.parentNode.insertBefore(li, ul) // same like the before()
-//         // ul.parentNode.insertBefore(li, ul.nextSibling) // same like the after()
-//         textFieldValue.value = '';
-//         // console.log(foodItems)
-//         // foodItems.forEach(element => {
-//         //         console.log('element',element)
-//         // });
-//         // console.log(foodItems[0].innerText)
-//         // const a = [].map.call(foodItems, (food) => food.textContent);
-//         // console.log(a)
-//         // foodItems.map(function(res) {
-//         //     console.log(res)
-//         // })
-//         // console.log(foodItems)
-//         createAlertViaDom();
-//         attachItemRemoveListener();
-//     }
-// }
-// function attachItemRemoveListener() {
-//     const clicks = Array.from(document.querySelectorAll('.click'));
-//     clicks.map((el) => {
-//         el.addEventListener('click', function (event) {
-//             const elRemove = event.target.parentNode;
-//             elRemove.remove(); //new way;
-//             // elRemove.parentNode.removeChild(elRemove); //old way
-//         });
-//     });
-// }
-// attachItemRemoveListener();
+let values = [];
+let textFieldValue = document.getElementById('text-value');
+let ul = document.getElementById('answershow')
+let foodItems = ul.getElementsByClassName('food-item')
+// console.log('ul', ul.parentNode.insertBefore(li, ul))
+const clearEl = document.getElementById('clear');
+
+clearEl.addEventListener('click', (event) => {
+    clearEl.classList.add('hide1')
+    textFieldValue.value='';
+})
+function printValue() {
+    if (textFieldValue.value) {
+        clearEl.classList.add('hide1')
+        let li = document.createElement('li');
+        let span = document.createElement('span');
+        let div = document.createElement('div');
+        div.innerText = 'X';
+        div.className = 'click';
+        // div.setAttribute('onclick', 'removeItem(event)'); //for clicking attribure
+        span.innerText = textFieldValue.value;
+        li.classList = 'food-item';
+        li.append(span, div);
+        // li.appendChild(document.createTextNode(textFieldValue.value))
+        // ul.appendChild(li);
+        ul.append(li);// append means adding element to lastchild
+        // ul.prepend(li) //prepend means adding element to firstchild
+        // ul.before(li) //adding element before the any element
+        // ul.after(li) // adding element after the any element
+        // ul.parentNode.insertBefore(li, ul) // same like the before()
+        // ul.parentNode.insertBefore(li, ul.nextSibling) // same like the after()
+        textFieldValue.value = '';
+        // console.log(foodItems)
+        // foodItems.forEach(element => {
+        //         console.log('element',element)
+        // });
+        // console.log(foodItems[0].innerText)
+        // const a = [].map.call(foodItems, (food) => food.textContent);
+        // console.log(a)
+        // foodItems.map(function(res) {
+        //     console.log(res)
+        // })
+        // console.log(foodItems)
+        createAlertViaDom();
+        attachItemRemoveListener();
+    }
+}
+
+textFieldValue.addEventListener('keyup', (event) => {
+    if(event.key == "Enter" && textFieldValue.value.length > 0) {
+        printValue();
+    }
+    if(textFieldValue.value && clearEl.classList.contains('hide1')) {
+        clearEl.classList.remove('hide1')
+    }else if(!(textFieldValue.value.length > 0)) {
+        clearEl.classList.add('hide1')
+    }
+})
+
+function attachItemRemoveListener() {
+    const clicks = Array.from(document.querySelectorAll('.click'));
+    clicks.map((el) => {
+        el.addEventListener('click', function (event) {
+            const elRemove = event.target.parentNode;
+            elRemove.remove(); //new way;
+            // elRemove.parentNode.removeChild(elRemove); //old way
+        });
+    });
+}
+attachItemRemoveListener();
 
 // // https://stackoverflow.com/questions/222841/most-efficient-way-to-convert-an-htmlcollection-to-an-array
 
@@ -76,102 +95,102 @@
 
 // // getelement it returns the html element in collection
 
-// // but in query selector it gives node list
+// but in query selector it gives node list
 
-// // <section class="alert">
-// // <h2>Succesfully updated</h2>
-// // </section>
+// <section class="alert">
+// <h2>Succesfully updated</h2>
+// </section>
 
-// // const content = document.getElementById('content');
-// // const alertBox = document.getElementById('alertBox');
-// // console.log('content', content, alertBox);
-// // const div = document.createElement('div');
-// // div.className = "alert-success"
-// // const textNode = document.createTextNode('successfully updated!')
-// // div.append(textNode)
-// // // div.textContent = "successfully  updated!"
-// // // div.innerText = "successfully updated!"
-// // // console.log('div', div)
-// // // content.append(div) //for append to last child
-// // content.prepend(div)
+// const content = document.getElementById('content');
+// const alertBox = document.getElementById('alertBox');
+// console.log('content', content, alertBox);
+// const div = document.createElement('div');
+// div.className = "alert-success"
+// const textNode = document.createTextNode('successfully updated!')
+// div.append(textNode)
+// // div.textContent = "successfully  updated!"
+// // div.innerText = "successfully updated!"
+// // console.log('div', div)
+// // content.append(div) //for append to last child
+// content.prepend(div)
 
-// function createAlertViaDom() {
-//     const content = document.getElementById('content');
-//     const alertBox = document.getElementById('alertBox');
-//     const div = document.createElement('div');
-//     div.className = "alert-success";
-//     div.id = "alert-success";
-//     const textNode = document.createTextNode('successfully updated!')
-//     div.append(textNode);
-//     alertBox.prepend(div);
-//     setTimeout(() => {
-//         alertBox.removeChild(document.getElementById('alert-success'))
-//     }, 2000)
-// }
-// // console.time('normal way start')
-// // // for(let i = 1; i < 1001; i++) {
-// // //     const liel = document.createElement('li');
-// // //     liel.textContent = `food item ${i}`;
-// // //     liel.classList.add('food-item')
-// // //     ul.append(liel);
-// // // }
-// // console.timeEnd('normal way start')
-// // create fragement document
-// // console.time('fragment way start')
-// // const fragment = document.createDocumentFragment();
+function createAlertViaDom() {
+    const content = document.getElementById('content');
+    const alertBox = document.getElementById('alertBox');
+    const div = document.createElement('div');
+    div.className = "alert-success";
+    div.id = "alert-success";
+    const textNode = document.createTextNode('successfully updated!')
+    div.append(textNode);
+    alertBox.prepend(div);
+    setTimeout(() => {
+        alertBox.removeChild(document.getElementById('alert-success'))
+    }, 2000)
+}
+// console.time('normal way start')
 // // for(let i = 1; i < 1001; i++) {
 // //     const liel = document.createElement('li');
 // //     liel.textContent = `food item ${i}`;
 // //     liel.classList.add('food-item')
-// //     fragment.append(liel);
+// //     ul.append(liel);
 // // }
-// // console.dir(fragment);
-// // ul.append(fragment)
-// // console.timeEnd('fragment way start')
+// console.timeEnd('normal way start')
+// create fragement document
+// console.time('fragment way start')
+// const fragment = document.createDocumentFragment();
+// for(let i = 1; i < 1001; i++) {
+//     const liel = document.createElement('li');
+//     liel.textContent = `food item ${i}`;
+//     liel.classList.add('food-item')
+//     fragment.append(liel);
+// }
+// console.dir(fragment);
+// ul.append(fragment)
+// console.timeEnd('fragment way start')
 
-// // insertAdjacentHTMl
-// // ul.insertAdjacentHTML('afterbegin', '<li class="food-item">afterbegin</li>');
-// // ul.insertAdjacentHTML('afterend', '<p>afterend</p>');
-// // ul.insertAdjacentHTML('beforebegin', '<p>beforeBegin</p>');
-// // ul.insertAdjacentHTML('beforeend', '<li class="food-item">beforeend</li>');
+// insertAdjacentHTMl
+// ul.insertAdjacentHTML('afterbegin', '<li class="food-item">afterbegin</li>');
+// ul.insertAdjacentHTML('afterend', '<p>afterend</p>');
+// ul.insertAdjacentHTML('beforebegin', '<p>beforeBegin</p>');
+// ul.insertAdjacentHTML('beforeend', '<li class="food-item">beforeend</li>');
 
-// // let li = document.createElement('li');
-// // li.textContent = 'contain li of insertAdjacentElement'
-// // li.classList.add('food-item')
+// let li = document.createElement('li');
+// li.textContent = 'contain li of insertAdjacentElement'
+// li.classList.add('food-item')
 
-// //insertAdjacentElement
-// // ul.insertAdjacentElement('afterbegin', li);
-// // ul.insertAdjacentElement('afterend', li);
-// // ul.insertAdjacentElement('beforebegin', li);
-// // ul.insertAdjacentElement('beforeend', li);
+// insertAdjacentElement
+// ul.insertAdjacentElement('afterbegin', li);
+// ul.insertAdjacentElement('afterend', li);
+// ul.insertAdjacentElement('beforebegin', li);
+// ul.insertAdjacentElement('beforeend', li);
 
-// // insertAdjacentText
-// // ul.insertAdjacentText('afterbegin', 'hi afterbegin');
-// // ul.insertAdjacentText('afterend', 'hi afterend');
-// // ul.insertAdjacentText('beforebegin', 'hi beforebegin');
-// // ul.insertAdjacentText('beforeend', 'hi beforeend');
+// insertAdjacentText
+// ul.insertAdjacentText('afterbegin', 'hi afterbegin');
+// ul.insertAdjacentText('afterend', 'hi afterend');
+// ul.insertAdjacentText('beforebegin', 'hi beforebegin');
+// ul.insertAdjacentText('beforeend', 'hi beforeend');
 
-// // const newLi = document.createElement('li');
-// // newLi.innerText = 'this from replacement text';
-// // newLi.className = 'food-item'
+// const newLi = document.createElement('li');
+// newLi.innerText = 'this from replacement text';
+// newLi.className = 'food-item'
 
-// // const replaceEl = document.querySelector('#answershow :first-child');
-// // console.log('replace', replaceEl)
-// // replaceEl.replaceWith(newLi) //new way
-// // replaceEl.parentElement.replaceChild(newLi, replaceEl)
-// // ul.replaceChildren('amount', 'money'); // its still this string not the node.
-// // if we want the node, we need to create a object
-// // neali = document.createElement('li');
-// // neali.innerText = 'node of object';
-// // ul.replaceChildren(neali); //this node object of element
-// // ul.replaceWith('amount', 'money') //it replace the current element with another node or el
+// const replaceEl = document.querySelector('#answershow :first-child');
+// console.log('replace', replaceEl)
+// replaceEl.replaceWith(newLi) //new way
+// replaceEl.parentElement.replaceChild(newLi, replaceEl)
+// ul.replaceChildren('amount', 'money'); // its still this string not the node.
+// if we want the node, we need to create a object
+// neali = document.createElement('li');
+// neali.innerText = 'node of object';
+// ul.replaceChildren(neali); //this node object of element
+// ul.replaceWith('amount', 'money') //it replace the current element with another node or el
 
 
 
-// // cloneNode() default false so they give clone of parent element only
-// // if we need a children also we need cloneNode(true)
-// // const cloneNode = ul.cloneNode(true);
-// // console.log('cloneNode', cloneNode)
+// cloneNode() default false so they give clone of parent element only
+// if we need a children also we need cloneNode(true)
+// const cloneNode = ul.cloneNode(true);
+// console.log('cloneNode', cloneNode)
 
 
 // // Deleting the remove ele
--- assets/mouse.js
+++ assets/mouse.js
@@ -30,16 +30,41 @@
 //     console.log('mouse over', event);
 // })
 
-document.addEventListener('mouseup', (event) => {
-    switch (event.which) {
-        case 1:
-            alert('you clicked left mouse button');
-            break;
-        case 2:
-            alert('you clicked middle mouse button');
-            break;
-        case 3:
-            alert('you clicked right mouse button');
-            break;
-    }
-});
+// document.addEventListener('mouseup', (event) => {
+//     switch (event.which) {
+//         case 1:
+//             alert('you clicked left mouse button');
+//             break;
+//         case 2:
+//             alert('you clicked middle mouse button');
+//             break;
+//         case 3:
+//             alert('you clicked right mouse button');
+//             break;
+//     }
+// });
+
+// keyboard events
+
+// document.addEventListener('keydown', (event) => {
+//     console.log('keydown', event);
+// });
+
+// document.addEventListener('keyup', (event) => {
+//     console.log('event', event);
+//     switch(event.code) {
+//         case 'ArrowUp':
+//             alert('you entered the up arrow button')
+//         break;
+//         case 'ArrowLeft':
+//             alert('you entered the ArrowLeft button')
+//         break;
+//         case 'ArrowDown':
+//             alert('you entered the ArrowDown button')
+//         break;
+//         case 'ArrowRight':
+//             alert('you entered the ArrowRight button')
+//         break;
+
+//     }
+// });
